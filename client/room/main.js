63fa6144601dcdebc9fc6593ff50cc1cd664f200
Hedgy134117 <hedgy134117@gmail.com>
2022-06-26 14:57:21 -0400

    syncing id + syncing users

--- client/room/main.js
+++ client/room/main.js
@@ -1,25 +1,40 @@
 let ws = new WebSocket("ws://127.0.0.1:8765/");
 let params = new URLSearchParams(window.location.search);
+let isAdmin = params.get("admin") == "true";
+let serverId = params.get("id");
 
 ws.onopen = function (e) {
     console.log("Working");
 
-    if (params.get("admin") == "true") {
+    if (isAdmin) {
         ws.send(JSON.stringify({ "action": "createServer" }));
     }
-    else if (params.get("admin") == "false" && params.get("id") != undefined) {
-        ws.send(JSON.stringify({ "action": "joinServer", "serverId": params.get("id") }));
+    else if (serverId != undefined) {
+        ws.send(JSON.stringify({ "action": "joinServer", "serverId": serverId }));
     }
 }
 
-// document.querySelector("button").onclick = () => {
-//     ws.send(JSON.stringify({ "message": "a" }));
-// }
-
 ws.onmessage = function (e) {
     console.log(`[SERVER] ${e.data}`);
+    let data = JSON.parse(e.data);
+    switch (data["type"]) {
+        case "id":
+            updateId(data["id"]);
+            break;
+        case "users":
+            updateUsers(data["users"]);
+            break;
+    }
 }
 
 ws.onclose = function (e) {
     ws.send(JSON.stringify({ "action": "closeServer" }))
+}
+
+function updateUsers(users) {
+    document.querySelector("#users").innerText = users;
+}
+
+function updateId(id) {
+    document.querySelector("#id").innerText = id;
 }
--- server/main.py
+++ server/main.py
@@ -10,33 +10,52 @@ SERVERS = []
 SERVER_IDS = []
 
 
-async def closeSameAdminServers(admin: WebSocketServerProtocol):
-    for server in SERVERS:
-        for serverData in server.values():
-            if serverData["admin"] == admin:
-                SERVERS.remove(server)
-                return
-
-
-async def createServer(admin: WebSocketServerProtocol):
-    await closeSameAdminServers(admin)
-
-    serverId = round(random.random() * 10000)
-    while serverId in SERVER_IDS:
-        serverId = round(random.random() * 10000)
-    SERVERS.append({str(serverId): {"admin": admin, "users": []}})
-    SERVER_IDS.append(serverId)
-
-    print(f"[createServer] New server created: ID '{serverId}', admin '{admin.id}'")
-
-
-async def joinServer(serverId: str, user: WebSocketServerProtocol):
+async def getServer(serverId: str) -> dict:
     for server in SERVERS:
         for serverListId in server:
             if serverListId == serverId:
-                server[serverListId]["users"].append(user)
-                print(f"[joinServer] User '{user.id}' joined Server '{serverId}'")
-                return
+                return server[serverListId]
+
+
+async def sendMessageToUsers(server: dict, message: str):
+    for user in server["users"]:
+        await user.send(message)
+
+
+async def createServer(admin: WebSocketServerProtocol):
+    serverId = round(random.random() * 10000)
+    while serverId in SERVER_IDS:
+        serverId = round(random.random() * 10000)
+    serverId = str(serverId)
+    SERVERS.append({serverId: {"admin": admin, "users": [admin]}})
+    SERVER_IDS.append(serverId)
+
+    print(f"[createServer] New server created: ID '{serverId}', admin '{admin.id}'")
+    await syncId(serverId)
+
+
+async def joinServer(serverId: str, user: WebSocketServerProtocol):
+    server = await getServer(serverId)
+    server["users"].append(user)
+    print(f"[joinServer] User '{user.id}' joined Server '{serverId}'")
+    await syncUsers(serverId)
+    await syncId(serverId)
+
+
+async def syncId(serverId: str):
+    server = await getServer(serverId)
+    message = json.dumps({"type": "id", "id": serverId})
+    await sendMessageToUsers(server, message)
+
+
+async def syncUsers(serverId: str):
+    server = await getServer(serverId)
+    userIds = []
+    for user in server["users"]:
+        userIds.append(str(user.id))
+
+    message = json.dumps({"type": "users", "users": userIds})
+    await sendMessageToUsers(server, message)
 
 
 async def main(websocket: WebSocketServerProtocol, path):
@@ -46,8 +65,6 @@ async def main(websocket: WebSocketServerProtocol, path):
 
         if data["action"] == "createServer":
             await createServer(websocket)
-        if data["action"] == "closeServer":
-            await closeSameAdminServers(websocket)
         if data["action"] == "joinServer":
             await joinServer(data["serverId"], websocket)
 
